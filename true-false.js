f43f78b6bcb81ac38a275dc49ba960377d343062
rakin-ahmed <rakin1912@gmail.com>
2020-03-22 13:27:17 +0600

    truefalse

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// false....
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// truth.....
+// "0" " " []
+
+// const name = "Rakin Ahmed"
+let name = null;
+console.log(name);
+
+if(name){
+    console.log("condition is true");
+    //if(name || name==0) 
+}
+else{
+    console.log("condition is false");
+    
+}
+//  any value other than 0 is true; 0 is false
