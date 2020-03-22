f06418c02418dda0ca2aae30445fa18e377cc3a4
rakin-ahmed <rakin1912@gmail.com>
2020-03-22 13:40:10 +0600

    nullvsundefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+let name;
+
+function add(num1,num2){
+    console.log(num1+num2); 
+    return    
+}
+
+function add(num1,num2){
+
+    console.log(num1,num2);
+    
+}
+
+
+
+const result= {name:"rakin", phone:4546}
+console.log(result.address);
+
+let fun = undefined
+log
+
+let ages = [3,4,5,]
+console.log(ages[34]);
+
+null 
